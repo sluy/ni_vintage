dbc9a4f0424bb7d6abea773b03a22e9e0ea01e66
Stefan Luy <sluy1283@gmail.com>
2023-02-27 17:35:53 -0400

    names to utf8mb4

--- bootstrap/db.php
+++ bootstrap/db.php
@@ -1,2 +1,7 @@
 <?php
-$db = new \PDO('mysql:host=' . $cfg['db']['host'] . ';dbname=' . $cfg['db']['name'], $cfg['db']['user'], $cfg['db']['pass']);
+$db = new \PDO(
+    'mysql:host=' . $cfg['db']['host'] . ';dbname=' . $cfg['db']['name'] . ';charset=utf8mb4',
+    $cfg['db']['user'],
+    $cfg['db']['pass']
+);
+$db->exec("SET NAMES utf8mb4");
