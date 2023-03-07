f7b5ec7988fdd0549fc33749e36149281f448135
Stefan Luy <sluy1283@gmail.com>
2023-03-07 12:39:00 -0400

    Company url parse & typecheck

--- models/company.php
+++ models/company.php
@@ -2,4 +2,17 @@
 
 class Company extends Model {
     public $tableName = T_COMPANIES;
+
+
+
+    public function get_pagina_web_value($raw) {
+        if (is_string($raw)) {
+            $raw = trim($raw);
+            if (!empty($raw) && filter_var($raw, FILTER_VALIDATE_URL)) {
+                return $raw;
+            }
+
+        }
+        return null;
+    }
 }
